0b655d6a022368b17e628b0178f375cf6a8578d1
joao-oliveira-neto <47464945+joao-oliveira-neto@users.noreply.github.com>
2019-05-01 01:04:02 -0300

    Add files via upload

---
+++ prjrede1/salario/src/salario/HoraExtra.java
@@ -0,0 +1,12 @@
+package salario;
+
+public class HoraExtra {
+
+	public static void main(String[] args) {
+		double h_extra =  8 * 1.5;
+		float sal = 8 * 160;
+		double total = 20 * h_extra + sal ;
+		System.out.println("Felipe receberá um total de:" + total  );
+	}
+
+}
